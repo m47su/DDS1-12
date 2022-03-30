6612375d4e77195fa09e5fcb20c489fbf37a09be
m47su <yasmin.siqueira@es.estudante.senai.br>
2022-03-30 16:50:48 -0300

    batatinha

---
+++ src/exercicios/Exercicio19.java
@@ -0,0 +1,19 @@
+package exercicios;
+import java.util.Scanner;
+public class Exercicio19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor do seu produto: ");
+        Double valorProduto = scanner.nextDouble();
+        System.out.print("Digite o tipo do pagamento [1 = à vista / 2 = a prazo]: ");
+        Integer tipoPagamento = scanner.nextInt();
+        Boolean pagamentoAVista = tipoPagamento.equals(1);
+        Double juros = 0.0;
+        if (!pagamentoAVista) {
+            juros = 10.0;}
+        Double acrescimo = valorProduto * juros / 100;
+        Double valorTotal = acrescimo + valorProduto;
+        System.out.println("Valor total: " + valorTotal);
+        scanner.close();
+    }
+}
