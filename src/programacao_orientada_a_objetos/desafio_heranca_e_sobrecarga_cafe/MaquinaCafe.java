8e939c454db50991dcfa00ded6edaa27a2a8d296
m47su <yasmin.siqueira@es.estudante.senai.br>
2022-06-15 13:56:07 -0300

    exercicios

--- src/programacao_orientada_a_objetos/desafio_heranca_e_sobrecarga_cafe/MaquinaCafe.java
+++ src/programacao_orientada_a_objetos/desafio_heranca_e_sobrecarga_cafe/MaquinaCafe.java
@@ -2,30 +2,18 @@ package programacao_orientada_a_objetos.desafio_heranca_e_sobrecarga_cafe;
 
 public class MaquinaCafe {
 
-    private int acucarDisponivel = 1000;
+    int acucarDisponivel;
 
+    void fazerCafe() {
+        fazerCafe(10);
+    }
 
-    int acucarDesejado = 50;
-
-    Boolean quantidadeDeAcucar;
-
-    public void fazerCafe() {
-
-        if(quantidadeDeAcucar) {
-
-            int resultado = (acucarDisponivel - acucarDesejado);
-
-            Boolean daPraFazer = resultado < acucarDisponivel;
-
-            if(daPraFazer) {
-
-                System.out.println("Seu café está sendo pronto com a quantidade " + acucarDesejado + "gramas." + "Ainda têm " + resultado + " gramas de açúcar");
-
-            }
-            else if (quantidadeDeAcucar)
-                System.out.println("A quantidade de café que você deseja é indisponível no momento");
-
-
+    void fazerCafe(int quantidadeAcucar) {
+        if (acucarDisponivel < quantidadeAcucar) {
+            System.out.println("Não há açucar suficiente para fazer seu café.");
+        } else {
+            acucarDisponivel -= quantidadeAcucar;
+            System.out.println("Fazendo cafezinho com " + quantidadeAcucar + " gramas de açucar.");
         }
     }
 }
--- src/programacao_orientada_a_objetos/desafio_heranca_e_sobrecarga_cafe/ServirCafe.java
+++ src/programacao_orientada_a_objetos/desafio_heranca_e_sobrecarga_cafe/ServirCafe.java
@@ -1,8 +1,11 @@
 package programacao_orientada_a_objetos.desafio_heranca_e_sobrecarga_cafe;
 public class ServirCafe{
-    public static void main(String[] args) {
-        MaquinaCafe maquinaCafe =  new MaquinaCafe();
-        maquinaCafe.fazerCafe();
-        }
-    }
 
+    public static void main(String[] args) {
+        MaquinaCafe maquina = new MaquinaCafe();
+        maquina.acucarDisponivel = 30;
+        maquina.fazerCafe(10);
+        maquina.fazerCafe(15);
+        maquina.fazerCafe();
+    }
+}
